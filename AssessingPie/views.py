0fafcc7bb02c3ea1994fa088e2fbcd642103ca65
anks315 <anks.315@gmail.com>
2014-08-23 23:25:24 +0530

    formula added Ankur Jain

--- AssessingPie/views.py
+++ AssessingPie/views.py
@@ -36,34 +36,42 @@ def asknextquestion(request):
         else:
             update(session['index'], False)
 
-    if  session.get('index',-1)!=-1 and (usersdict[session['index']].states[maxstate(session['index'])].prob > 0.70 or getcount(session['index'])==3 ):
-        global counter
-        counter=0
+    logging.error(usersdict[session['index']].states[maxstate(session['index'])].questionstuple)
+    val=0
+    if  session.get('index',-1)!=-1 and (usersdict[session['index']].states[maxstate(session['index'])].prob > 0.40 or getcount(session['index'])==3 ):
         global val
         strknow = "You know :"
-        val =0;
-        while(counter<3):
-            if counter in usersdict[session['index']].states[maxstate(session['index'])].questionstuple:
-                val+=1
-
-                strknow+=usersdict[session['index']].questions[counter].questionstring
+        for quest in usersdict[session['index']].states[maxstate(session['index'])].questionstuple:
+            if quest==-1:
+                val=0
+            else:
+                strknow+=usersdict[session['index']].questions[quest].questionstring
                 strknow+=",,"
-            counter+=1
+                val+=1
         maxsta =maxstate(session['index'])
-        if val!=3:
-            readytolearn = usersdict[session['index']].questions[val].questionstring
+        if val==3:
+
+            readytolearn="Congratulations!!you have completed maths donut"
         else:
-            readytolearn = 'Congratulation You have completed the course'
+            logging.error(maxstate(session['index']))
+            readytolearn=usersdict[session['index']].questions[usersdict[session['index']].states[maxstate(session['index'])+1].questionstuple[maxstate(session['index'])]].questionstring
         del usersdict[session['index']]
         del session['index']
-        return render_to_response('AssessingPie/pie.html',{'num_known': val ,'num_dontknow':3-val,'st': maxsta,'state' : 'completed','know':strknow,'ready_to_learn':readytolearn},context_instance = RequestContext(request))
-    else:
+        return render_to_response('AssessingPie/pie.html',{'readytolearn':readytolearn,'num_known': val ,'num_dontknow':3-val,'st': maxsta,'state' : 'completed','know':strknow,},context_instance = RequestContext(request))
 
+    else:
         return render_to_response('AssessingPie/question.html',{'logouturl':users.create_logout_url('/') ,'logger' : users.get_current_user() ,'nextquestion' : usersdict[session['index']].questions[nextquestion(session['index'])].questionstring,},context_instance = RequestContext(request))
 
 def home(request):
+
+
      session = get_current_session()
      if session.get('index',-1) != -1:
         del session['index']
 
-     return render_to_response('AssessingPie/home.html',{'loginurl': users.create_login_url('/'),},context_instance = RequestContext(request))
+     return render_to_response('AssessingPie/abc.html',{'loginurl': users.create_login_url('/'),},context_instance = RequestContext(request))
+
+
+def contactus(request):
+
+        return render_to_response('AssessingPie/contact.html',{},context_instance = RequestContext(request))
