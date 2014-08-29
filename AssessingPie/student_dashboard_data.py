f0ca1a3681f61c7b418d351d9d021bb2dcc4e0c0
anks315 <anks.315@gmail.com>
2014-08-29 20:22:10 +0530

    dummy data Ankur Jain

--- AssessingPie/student_dashboard_data.py
+++ AssessingPie/student_dashboard_data.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.template import RequestContext,loader,Context
 import Query
 import Constant
 from gaesessions import get_current_session
@@ -21,7 +21,11 @@ def userdetails(request):
         session['student']= student
         user_name =  student.basic_info.firstname + ' ' + student.basic_info.lastname
         last_login = user_information[2]
-        return render_to_response('Dashboard/user_information.xml',{'user_name':user_name,'last_login':last_login},context_instance = RequestContext(request),content_type="text/xml;")
+        t = loader.get_template('Dashboard/user_information.xml')
+        c = Context({'user_name':user_name,'last_login':last_login})
+        return HttpResponse(t.render(c),
+        content_type="text/xml")
+        #return render_to_response('Dashboard/user_information.xml',{'user_name':user_name,'last_login':last_login},context_instance = RequestContext(request),content_type="text/xml;")
 
 def getmasterybysubject(request):
     user_information= Query.login("Ankit_Bhatia","ankit")
@@ -29,7 +33,12 @@ def getmasterybysubject(request):
     student = user_information[1]
     session['student']= student
     mastery = Query.get_mastery_for_all_subjects(session['student'].key)
-    return render_to_response('Dashboard/mastery_by_subject.xml',{'masterdict':mastery,},context_instance = RequestContext(request),content_type="text/xml")
+    t = loader.get_template('Dashboard/mastery_by_subject.xml')
+    c = Context({'masterdict':mastery,})
+    return HttpResponse(t.render(c),
+    content_type="text/xml")
+
+    #return render_to_response('Dashboard/mastery_by_subject.xml',{'masterdict':mastery,},context_instance = RequestContext(request),content_type="text/xml")
 
 def getpendingassessment(request):
     user_information= Query.login("Ankit_Bhatia","ankit")
@@ -41,7 +50,11 @@ def getpendingassessment(request):
     for subject in subjectlist:
         pendingassessments = Query.get_pending_assessment_subject(subject.key,session['student'].key)
         pendingassessmentbysubject[subject.name]= pendingassessments
-        return render_to_response('Dashboard/pending_assessmentbysubject.xml',{'assessmentdict':pendingassessmentbysubject,},context_instance = RequestContext(request),content_type="text/xml")
+    t = loader.get_template('Dashboard/pending_assessmentbysubject.xml')
+    c = Context({'assessmentdict':pendingassessmentbysubject,})
+    return HttpResponse(t.render(c),
+    content_type="text/xml")
+    #return render_to_response('Dashboard/pending_assessmentbysubject.xml',{'assessmentdict':pendingassessmentbysubject,},context_instance = RequestContext(request),content_type="text/xml")
 
 def getgrowthforallsubject(request):
     user_information= Query.login("Ankit_Bhatia","ankit")
@@ -49,7 +62,11 @@ def getgrowthforallsubject(request):
     student = user_information[1]
     session['student']= student
     growthdict = Query.get_growth_for_all_subject(session['student'].key)
-    return render_to_response('Dashboard/growth_by_subject.xml',{'growthdict':growthdict,},context_instance = RequestContext(request),content_type="text/xml")
+    t = loader.get_template('Dashboard/growth_by_subject.xml')
+    c = Context({'growthdict':growthdict,})
+    return HttpResponse(t.render(c),
+    content_type="text/xml")
+    #return render_to_response('Dashboard/growth_by_subject.xml',{'growthdict':growthdict,},context_instance = RequestContext(request),content_type="text/xml")
 
 
 def getreadytolearnbysubject(request):
@@ -58,7 +75,21 @@ def getreadytolearnbysubject(request):
     student = user_information[1]
     session['student']= student
     readytolearndict =  Query.get_ready_to_learn_of_all_topic_dummy(session['student'].key)
+    t = loader.get_template('Dashboard/readytolearn_by_subject')
+    c = Context({'readytolearndict':readytolearndict,})
+    return HttpResponse(t.render(c),
+    content_type="text/xml")
+    #return render_to_response('Dashboard/readytolearn_by_subject',{'readytolearndict':readytolearndict,},context_instance = RequestContext(request),content_type="text/xml")
 
-    return render_to_response('Dashboard/readytolearn_by_subject',{'readytolearndict':readytolearndict,},context_instance = RequestContext(request),content_type="text/xml")
 
-
+def getlearningprogressdatewise(request):
+    user_information= Query.login("Ankit_Bhatia","ankit")
+    session = get_current_session()
+    student = user_information[1]
+    session['student']= student
+    learningprogressdict = Query.get_learning_progress_date_wise_dummy(session['student'].key)
+    t = loader.get_template('Dashboard/learningprogress_by_date')
+    c = Context({'learningprogressdict':learningprogressdict,})
+    return HttpResponse(t.render(c),
+    content_type="text/xml")
+    #return render_to_response('Dashboard/learningprogress_by_date',{'learningprogressdict':learningprogressdict,},context_instance = RequestContext(request))
