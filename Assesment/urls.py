44529832e568f16ebbd2dc8778faa0cd6f39534f
anks315 <anks.315@gmail.com>
2014-08-31 01:16:57 +0530

    dummy data Ankur Jain

--- Assesment/urls.py
+++ Assesment/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Assesment.views.home', name='home'),
     # url(r'^Assesment/', include('Assesment.foo.urls')),
-    (r'^assessment/$', 'AssessingPie.views.asknextquestion'),
+    (r'^assessment$', 'AssessingPie.views.asknextquestion'),
     (r'^inference/$', 'AssessingPie.views.inferquestion'),
     (r'^contact_us/$', 'AssessingPie.views.contactus'),
     (r'^topicnames/$', 'AssessingPie.views.topicnames'),
